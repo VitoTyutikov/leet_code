a793b86c1e2f0f0805cd73c48b2da6ceeeb9524d
shieldbr <shieldbr@student.21-school.ru>
2023-09-27 13:34:09 +0300

    day 4

---
+++ src/main/java/org/lc/LeetCode_75/Task6.java
@@ -0,0 +1,24 @@
+package org.lc.LeetCode_75;
+
+import java.util.ArrayList;
+
+public class Task6 {
+    public static void main(String[] args) {
+        System.out.println("'"+reverseWords("  hello world  ")+"'");
+    }
+
+    public static String reverseWords(String s) {
+        String result = new String();
+        String[] strings = s.split(" ");
+        for (int i = strings.length - 1; i >= 0; i--) {
+            if (!strings[i].equals(" ") && !strings[i].equals("\n")&&!strings[i].equals("")) {
+                    result += strings[i] + " ";
+            }
+        }
+        if(result.charAt(result.length()-1)==' '){
+            result=result.substring(0, result.length()-1);
+        }
+
+        return result;
+    }
+}
